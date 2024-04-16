713823f081e4cb278de890b44fb55ea5a13b42a9
PuckInside <saginov606@gmail.com>
2024-04-16 08:45:51 +0500

    Merge with YandexTask

---
+++ emo/emo/emo.cpp
@@ -0,0 +1,94 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+std::string GetInput(std::string path)
+{
+    std::string line;
+    std::ifstream in(path);
+
+    if (in.is_open())
+        std::getline(in, line);
+    else
+        line = "";
+
+    in.close();
+    return line;
+}
+
+void SendOutput(std::string path, bool output)
+{
+    std::ofstream out;
+    out.open(path);
+
+    if (out.is_open())
+    {
+        if (output)
+            out << "YES" << std::endl;
+        else
+            out << "NO" << std::endl;
+    }
+
+    out.close();
+}
+
+bool IsHasNumber(std::string value)
+{
+    for (int i = 0; i <= 9; i++)
+    {
+        if (value.find('0' + i) != std::string::npos)
+            return true;
+    }
+
+    return false;
+}
+
+bool IsHasUppercase(std::string value)
+{
+    for (int i = 0; i <= 25; i++)
+    {
+        if (value.find('A' + i) != std::string::npos)
+            return true;
+    }
+
+    return false;
+}
+
+bool IsHasLowercase(std::string value)
+{
+    for (int i = 0; i <= 25; i++)
+    {
+        if (value.find('a' + i) != std::string::npos)
+            return true;
+    }
+
+    return false;
+}
+
+bool IsCorrectFormat(std::string value)
+{
+    if (value.length() < 8)
+        return false;
+
+    if (!IsHasNumber(value))
+        return false;
+
+    if (!IsHasUppercase(value))
+        return false;
+
+    if (!IsHasLowercase(value))
+        return false;
+
+    return true;
+}
+
+int main()
+{
+    std::string nick = GetInput("input.txt");
+
+    bool output = IsCorrectFormat(nick);
+
+    SendOutput("output.txt" , output);
+
+    return 0;
+}
---
+++ fish/fish/fish.cpp
@@ -0,0 +1,162 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+std::vector<std::string> SplitString(std::string inputString)
+{
+    std::string currentWord;
+    std::vector<std::string> words;
+
+    for (char character : inputString)
+    {
+        if (character != ' ')
+        {
+            currentWord += character;
+        }
+        else
+        {
+            words.push_back(currentWord);
+            currentWord = "";
+        }
+    }
+
+    if (!currentWord.empty())
+        words.push_back(currentWord);
+
+    return words;
+}
+
+struct FishInfo
+{
+    float days = 0;
+    float shelfLife = 0;
+    std::vector<int> prices;
+};
+
+FishInfo GetInput(std::string path)
+{
+    std::string line;
+    std::ifstream in(path);
+    FishInfo info;
+
+    if (in.is_open())
+    {
+        int allPrices = 0;
+
+        std::getline(in, line);
+        std::vector<std::string> words = SplitString(line);
+
+        info.days = std::stoi(words[0]);
+        info.shelfLife = std::stoi(words[1]);
+
+        std::getline(in, line);
+        std::vector<std::string> prices = SplitString(line);
+
+        for (int i = 0; i < prices.size(); i++)
+            info.prices.push_back(std::stoi(prices[i]));
+    }
+    else
+    {
+        info.days = 0;
+        info.shelfLife = 0;
+    }
+
+    in.close();
+    return info;
+}
+
+void SendOutput(std::string path, std::string output)
+{
+    std::ofstream out;
+    out.open(path);
+
+    if (out.is_open())
+    {
+        out << output << std::endl;
+    }
+
+    out.close();
+}
+
+int CurrentPurchase(std::vector<int> interval, int balance)
+{
+    int current = interval[0];
+    int possible = 0;
+    int buy = 0;
+
+    for (int x : interval)
+    {
+        if (current <= x)
+            possible++;
+    }
+
+    buy = possible - balance;
+    if (buy <= 0)
+        return 0;
+
+    return buy;
+}
+
+std::vector<int> PurchaseForecast(FishInfo info)
+{
+    std::vector<int> purchases;
+    int balance = 0;
+
+    for (int i = 0; i < info.prices.size(); i++)
+    {
+        std::vector<int> interval;
+
+        for (int j = 0; j < info.shelfLife; j++)
+        {
+            if (i + j == info.prices.size() - 1)
+            {
+                interval.push_back(info.prices[i + j]);
+                break;
+            }
+            interval.push_back(info.prices[i + j]);
+        }
+
+        int current = CurrentPurchase(interval, balance);
+        purchases.push_back(current);
+        balance += current;
+        balance--;
+    }
+
+    return purchases;
+}
+
+int CalculateCost(std::vector<int> prices, std::vector<int> buys)
+{
+    int cost = 0;
+
+    for (int i = 0; i < prices.size(); i++)
+    {
+        cost += prices[i] * buys[i];
+    }
+    
+    return cost;
+}
+
+int main()
+{
+    FishInfo info = GetInput("input.txt");
+    std::vector<int> buys = PurchaseForecast(info);
+
+    int cost = CalculateCost(info.prices, buys);
+
+    std::string output;
+    output = std::to_string(cost);
+    output += "\n";
+    for (int i = 0; i < buys.size(); i++)
+    {
+        output += std::to_string(buys[i]);
+        output += " ";
+    }
+
+
+    std::cout << output;
+    //SendOutput("output.txt", output);
+
+    return 0;
+}
